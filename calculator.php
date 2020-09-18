0550ca9a73295ae703c0132a8cf525dc515539b1
Arr Belrey Domingo <59627816+snowman07@users.noreply.github.com>
2020-09-18 10:45:53 -0600

    modify some code esp option tag and switch statement

--- calculator.php
+++ calculator.php
@@ -3,7 +3,7 @@ Name: Arr Belrey Domingo
 Date: September 14, 2020 (Fall2020 Term)
 Instructor: Philip Redmond
 Course: DMIT 2025 PHP/MySQL
-Description: This is a Calculator form in PHP file. User need to input Num 1, operation(+,-,*,/), and Num 2. Result will show below the form. Value of the inputs will retain in the browser (prepopping the form) 
+Description: This is a Calculator form in PHP file. User need to input Num 1, operation(+,-,*,/), and Num 2. Result will show below the form. Value of the inputs will retain in the browser (prepopulate the form) 
 -->
 
 <!DOCTYPE html>
@@ -26,16 +26,16 @@ Description: This is a Calculator form in PHP file. User need to input Num 1, op
 
             //Let's do a switch conditional statement based on the type of operation
             switch ($operator) {
-                case "add": //this is the "value" attribute of "option" tag. Can also be the text string of "option" tag, enclose it with quatation marks
+                case "+": //this is the content of "option" tag, enclose in a text string. Can also be the "value" attribute of "option" tag. 
                     $result = $num1 + $num2;
                     break;
-                case "subtract": //this is the "value" attribute of "option" tag. Can also be the text string of "option" tag, enclose it with quatation marks
+                case "-": //this is the content of "option" tag, enclose in a text string. Can also be the "value" attribute of "option" tag.
                     $result = $num1 - $num2;
                     break;
-                case "multiply": //this is the "value" attribute of "option" tag. Can also be the text string of "option" tag, enclose it with quatation marks
+                case "*": //this is the content of "option" tag, enclose in a text string. Can also be the "value" attribute of "option" tag.
                     $result = $num1 * $num2;
                     break;
-                case "divide": //this is the "value" attribute of "option" tag. Can also be the text string of "option" tag, enclose it with quatation marks
+                case "/": //this is the content of "option" tag, enclose in a text string. Can also be the "value" attribute of "option" tag.
                     $result = $num1 / $num2;
                     break;
                 //default:
@@ -48,22 +48,27 @@ Description: This is a Calculator form in PHP file. User need to input Num 1, op
     <h1>Calculator</h1>
 
     <form name="calcform" method="post" action="calculator.php">
-        Num 1: <input type="number" name="num1" value="<?php echo $num1?>"> <!--"value" attrib is use to prepopping the form-->
+        Num 1: <input type="number" name="num1" value="<?php echo $num1?>"> <!--"value" attrib is use to prepopulate the form-->
         <select name="operator">
 
         <?php
-        // quick and dirty way to retain the value of operator "input" (prepopping the form of the name="operator")
+        // quick and dirty way to retain the value of operator "input" (prepopulate the form of the name="operator")
+        // not perfect as it is simply adds one option to the top, but better than losing the users last operation
         if($operator){
             echo "\n\t<option>$operator</option>"; 
         }
         ?>
 
-            <option value="add">+</option>
-            <option value="subtract">-</option>
-            <option value="multiply">*</option>
-            <option value="divide">/</option>
+            <!--<option value="add">+</option>-->
+            <option>+</option>
+            <!--<option value="subtract">-</option>-->
+            <option>-</option>
+            <!--<option value="multiply">*</option>-->
+            <option>*</option>
+            <!--<option value="divide">/</option>-->
+            <option>/</option>
         </select>
-        Num 2: <input type="number" name="num2" value="<?php echo $num2?>"> <!--"value" attrib is use to prepopping the form-->
+        Num 2: <input type="number" name="num2" value="<?php echo $num2?>"> <!--"value" attrib is use to prepopulate the form-->
         <input type="submit" name="mysubmit">
     </form>
 
